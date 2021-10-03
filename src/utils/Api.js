a98cac04138c0416c1f7983100498190bb1765fa
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-10-04 00:17:10 +0100

    initial commit

---
+++ src/utils/Api.js
@@ -0,0 +1,31 @@
+import { 
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer,
+} from './_DATA.js';
+
+export function getInitialData () {
+    return Promise.all([
+        _getUsers,
+        getQuestions,
+    ]).then(([users, questions]) => ({
+        users,
+        questions
+
+}))
+
+}
+
+export function _saveQuestion (info) {
+    return _saveQuestion(info)
+
+
+
+}
+
+
+export function _saveQuestionAnswer (info) {
+return _saveQuestionAnswer(info)
+}
+
---
+++ src/utils/helpers.js
@@ -0,0 +1,18 @@
+// format questions
+
+export function formatQuestion(question, author) {
+  const { id, optionOne, optionTwo } = question;
+  const { name, avatarURL } = author;
+
+  return { name, id, avatarURL, optionOne, optionTwo };
+
+
+//   format date
+  export function formatDate(timestamp) {
+      
+    const d = new Date(timestamp)
+    const time = d.toLocaleTimeString('en-US')
+    return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+  }
+  
+}
