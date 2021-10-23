81dcab1cc23b2de426f94b3c496515f628058050
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-10-23 13:49:43 +0100

    tweak to mapstatetoprops and general logic

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import Question from "./Question";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export class Dashboard extends Component {
   state = {
@@ -11,21 +11,22 @@ export class Dashboard extends Component {
   };
 
   render() {
-    const { users, questions, qid, signedInUser, optionOne, user} = this.props;
-    // const {showUnansweredQs, showAnsweredQs} = this.state
-console.log(optionOne)
-    const answeredQs = users[signedInUser] && Object.keys(users[signedInUser].answers).map((qid) => questions[qid])
-   
-    answeredQs && answeredQs.sort((a, b) => questions[b].timestamp - questions[a].timestamp);
- 
-    const unansweredQs = questions && Object.keys(questions)
-      .filter((question) => !answeredQs(question))
-      .includes(qid)
-      unansweredQs && unansweredQs.sort(
-      (a, b) => questions[b].timestamp - questions[a].timestamp
-    )
+    const { answeredQs, unansweredQs, user, id } = this.props;
+    //     const { users, questions, qid, signedInUser, id, optionOne, user} = this.props;
+    //     // const {showUnansweredQs, showAnsweredQs} = this.state
+    // console.log(users)
+    //     const answeredQs = users[signedInUser] && Object.keys(users[signedInUser].answers).map((qid) => questions[qid])
+
+    //     answeredQs && answeredQs.sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
+    //     const unansweredQs = questions && Object.keys(questions)
+    //       .filter((question) => !answeredQs(question))
+    //       .includes(qid)
+    //       unansweredQs && unansweredQs.sort(
+    //       (a, b) => questions[b].timestamp - questions[a].timestamp
+    //     )
     return (
-      <Tabs className='question-dashboard'>
+      <Tabs className="question-dashboard">
         <TabList>
           <Tab>Unanswered Questions</Tab>
           <Tab>Answered Questions</Tab>
@@ -33,56 +34,60 @@ console.log(optionOne)
 
         <TabPanel>
           <ul>
-            {unansweredQs && unansweredQs.map(id => (
-              <li key={id}>
-                <div>
-                  <p>{user.name} wants to know...</p>
-                  <Question
-                    id={id}
-                    userAvatar={user.AvatarURL}
-                    userName={user.name}
-                    questionState="unanswered"
-                    userQuestion={questions[optionOne.text]}
-                  />
-                
-                  <p>or...</p>
-                  <Link
-                    to={{
-                      pathname: "questions/:question_id",
-                      state: { answered: false },
-                    }}
-                  >
-                    <button>Answer Question</button>
-                  </Link>
-                </div>
-              </li>
-            ))} 
-          </ul> 
+            {unansweredQs &&
+              unansweredQs.map((question) => (
+                <li key={question.id}>
+                  <div>
+                    <p>{user.name} wants to know...</p>
+                    <Question
+                      qid={id}
+                      userAvatar={user.AvatarURL}
+                      userName={user.name}
+                      question={question}
+                      questionState="unanswered"
+                      // userQuestion={questions[optionOne.text]}
+                    />
+
+                    <p>or...</p>
+                    <Link
+                      to={{
+                        pathname: "questions/:question_id",
+                        state: { answered: false },
+                      }}
+                    >
+                      <button>Answer Question</button>
+                    </Link>
+                  </div>
+                </li>
+              ))}
+          </ul>
         </TabPanel>
         <TabPanel>
           <ul>
-            {answeredQs && answeredQs.map(qid => (
-              <li key={qid}>
-                <div>
-                  <p>{user.name} asks...</p>
-                  <Question
-                    id={qid}
-                    userAvatar={user.AvatarURL}
-                    userName={user.name}
-                    questionState="answered"
-                    userQuestion={questions[optionOne.text]}
-                  />
-                  <Link
-                    to={{
-                      pathname: "questions/:question_id",
-                      state: { answered: true },
-                    }}
-                  >
-                    <button>Results</button>
-                  </Link>
-                </div>
-              </li>
-            ))}
+            {answeredQs &&
+              answeredQs.map((question) => (
+                <li key={question.id}>
+                  <div>
+                    <p>{user.name} asks...</p>
+                    <Question
+                      qid={id}
+                      userAvatar={user.AvatarURL}
+                      userName={user.name}
+                      question={question}
+                      questionState="answered"
+                      // userQuestion={questions[optionOne.text]}
+                    />
+                    <Link
+                      to={{
+                        pathname: "questions/:question_id",
+                        state: { answered: true },
+                      }}
+                    >
+                      <button>Results</button>
+                    </Link>
+                  </div>
+                </li>
+              ))}
           </ul>
         </TabPanel>
       </Tabs>
@@ -90,14 +95,45 @@ console.log(optionOne)
   }
 }
 
-function mapStateToProps(state) {
-  console.log(state)
+// function mapStateToProps(state) {
+//   console.log(state)
+//   return {
+//     users: state.users,
+//     signedInUser: state.signedInUser,
+//     questions: state.questions,
+//   };
+// }
+
+function mapStateToProps({ users, signedInUser, questions }, { qid }) {
+  // const { users, questions, qid, signedInUser, id, optionOne, user} = this.props;
+  // const {showUnansweredQs, showAnsweredQs} = this.state
+
+  const answeredQs =
+    users[signedInUser] &&
+    Object.keys(users[signedInUser].answers).map((qid) => questions[qid]);
+
+  answeredQs &&
+    answeredQs.sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
+  const unansweredQs =
+    questions &&
+    Object.keys(questions)
+      .filter((question) => !answeredQs(question))
+      .includes(qid);
+  unansweredQs &&
+    unansweredQs.sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    );
+  const question = questions[qid];
+  const user = users[questions.author];
   return {
-    users: state.users,
-    signedInUser: state.signedInUser,
-    questions: state.questions,
+    users,
+    user,
+    signedInUser,
+    questions,
+    question,
+    qid,
   };
 }
-
 
 export default connect(mapStateToProps)(Dashboard);
