35322ce211d64f433e9fd4d68ed2a1f2b004ad85
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-01-09 22:05:37 +0000

    chore: tidy of styles filesystem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./styles/App.css";
 import './styles/style.css';
 import App from "./components/App";
 import { createStore } from "redux";
